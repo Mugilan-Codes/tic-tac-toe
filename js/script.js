1a2d6c5503f789b7867febbd50bdb957e08b3a41
Mugilan <mugilancodes@gmail.com>
2020-07-05 22:41:57 +0530

    Thinking of Logic

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 // Module
 const GameBoard = (function () {
   let gameBoard = [];
+
+  return { gameBoard };
 })();
 
 // Factory
-const CreatePlayer = (name, age) => {
-  return { name, age };
+const CreatePlayer = (name) => {
+  let score = 0;
+  return { name, score };
 };
 
-const player1 = CreatePlayer('Mugilan', 21);
-const player2 = CreatePlayer('Jay', 22);
+const player1 = CreatePlayer('Mugilan');
+const player2 = CreatePlayer('Jay');
 
 console.log({ player1, player2 });
