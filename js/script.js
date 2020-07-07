8011bdc4c379fe63725436272c276bf7c92adeba
Mugilan <mugilancodes@gmail.com>
2020-07-07 13:05:01 +0530

    Render the array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+const X_MOVE = 'X';
+const O_MOVE = 'O';
+
+const gridPositions__div = document.querySelectorAll('.grid-item');
+
 // Module
 const GameBoard = (function () {
-  let gameBoard = [];
+  let gameBoard = [
+    X_MOVE,
+    O_MOVE,
+    X_MOVE,
+    O_MOVE,
+    X_MOVE,
+    O_MOVE,
+    X_MOVE,
+    O_MOVE,
+    X_MOVE,
+  ];
 
   const render = () => {
     console.log(gameBoard);
+    gridPositions__div.forEach((pos) => {
+      pos.textContent = gameBoard[pos.dataset.position];
+    });
   };
 
   return { render };
@@ -20,3 +38,9 @@ const player2 = CreatePlayer('Jay');
 
 console.log({ player1, player2 });
 GameBoard.render();
+
+// console.log(gridPositions__div);
+
+// gridPositions__div.forEach((pos) =>
+//   pos.addEventListener('click', (e) => console.log(e.target.dataset.position))
+// );
