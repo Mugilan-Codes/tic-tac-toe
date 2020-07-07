a62ebd556e918766b90dac015badb866c4c80f36
Mugilan <mugilancodes@gmail.com>
2020-07-07 12:52:52 +0530

    Board Template

--- js/script.js
+++ js/script.js
@@ -20,12 +20,3 @@ const player2 = CreatePlayer('Jay');
 
 console.log({ player1, player2 });
 GameBoard.render();
-
-// Set Width of the Game Board
-const gameBoard__main = document.querySelector('main');
-
-const windowHeight = window.screen.height;
-const windowWidth = window.screen.width;
-
-console.log(gameBoard__main.clientHeight);
-console.log(gameBoard__main.clientWidth);
