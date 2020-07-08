dee36113c7bc4c28093cb34561793eefe236e280
Mugilan <mugilancodes@gmail.com>
2020-07-08 12:19:14 +0530

    ChangeOver

--- js/script.js
+++ js/script.js
@@ -1,48 +1,24 @@
-const gridPositions__div = document.querySelectorAll('.grid-item');
-
-// Module
 const GameBoard = (function () {
-  let gameBoard = ['', '', '', '', '', '', '', '', ''];
+  // ['', '', '', '', '', '', '', '', '']
+  // const gameBoard = Object.seal([...Array(9)].fill(''));
+  const gameBoard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
 
-  let lastMarker;
-
-  const _render = () => {
-    console.log(gameBoard);
-    gridPositions__div.forEach((pos) => {
-      pos.textContent = gameBoard[pos.dataset.position];
-    });
-  };
-
-  const setPiece = function (player) {
-    console.log(this);
-    gameBoard[this.dataset.position] = player2.placeMarker();
-    _render();
-  };
-
-  return { setPiece };
+  return { gameBoard };
 })();
 
-// Factory
-const CreatePlayer = (name, key) => {
-  const MOVES = ['X', 'O'];
-  let score = 0;
-
-  const placeMarker = () => {
-    return MOVES[key];
+const DisplayController = (function () {
+  const render = () => {
+    console.log(GameBoard.gameBoard);
   };
 
-  return { name, score, placeMarker };
-};
+  return { render };
+})();
 
-const player1 = CreatePlayer('Mugilan', 0);
-const player2 = CreatePlayer('Jay', 1);
+const PlayerFactory = (name) => name;
 
-player1.placeMarker();
-player2.placeMarker();
+DisplayController.render();
+
+const player1 = PlayerFactory('Mugilan');
+const player2 = PlayerFactory('Jay');
 
 console.log({ player1, player2 });
-// GameBoard.render();
-
-gridPositions__div.forEach((pos) =>
-  pos.addEventListener('click', GameBoard.setPiece)
-);
