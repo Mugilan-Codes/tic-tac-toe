00521d46fe29c793d394b8f3d7b3b8fc810e0745
Mugilan <mugilancodes@gmail.com>
2020-07-08 13:06:37 +0530

    click on board

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
+const gridItems = document.querySelectorAll('.grid-item');
+
 const GameBoard = (function () {
   // ['', '', '', '', '', '', '', '', '']
-  // const gameBoard = Object.seal([...Array(9)].fill(''));
-  const gameBoard = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+  const gameBoard = Object.seal([...Array(9)].fill(''));
 
-  const gridItems = document.querySelectorAll('.grid-item');
-
-  const render = () => {
+  const _render = () => {
     console.log(gameBoard);
     gridItems.forEach(
       (item) => (item.textContent = gameBoard[item.dataset.position])
     );
   };
 
-  return { render };
-})();
+  const clickOnBoard = () => {
+    gridItems.forEach((item) =>
+      item.addEventListener('click', function () {
+        // Returns if the cell is already filled
+        if (gameBoard[this.dataset.position]) return;
 
-const DisplayController = (function () {
-  const placeMarker = () => {};
+        gameBoard[this.dataset.position] = 'X';
+        _render();
+      })
+    );
+  };
 
-  return { placeMarker };
+  return { clickOnBoard };
 })();
 
 const PlayerFactory = (name, marker) => {
   return { name, marker };
 };
 
-GameBoard.render();
-
 const player1 = PlayerFactory('Mugilan', 'X');
 const player2 = PlayerFactory('Jay', 'O');
 
 console.log({ player1, player2 });
+
+GameBoard.clickOnBoard();
