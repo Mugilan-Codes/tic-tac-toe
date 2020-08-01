f6326cb6cc4362e93489983c7bcec5fd4ebae29d
Mugilan <mugilancodes@gmail.com>
2020-08-01 10:37:27 +0530

    Update Game

--- js/script.js
+++ js/script.js
@@ -16,7 +16,31 @@ const gameBoard = (function () {
     console.log(board);
   };
 
+  const checkWin = (marker) => {
+    const winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  };
+
   return { resetBoard, updateBoard };
 })();
 
-const displayController = (function () {})();
+const game = (function () {
+  const player1 = players('Mugilan', 'X');
+  const player2 = players('Samantha', 'O');
+
+  const start = () => {
+    console.log({ player1, player2 });
+  };
+
+  return { start, checkWin };
+})();
+
+game.start();
