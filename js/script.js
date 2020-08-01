8422df51c213eaecf396721b3928424d874ae0e4
Mugilan <mugilancodes@gmail.com>
2020-08-01 09:35:47 +0530

    Starting Fresh

--- js/script.js
+++ js/script.js
@@ -1,54 +1,17 @@
-const gridItems = document.querySelectorAll('.grid-item');
-
-const GameBoard = (function () {
-  // ['', '', '', '', '', '', '', '', '']
-  const gameBoard = Object.seal([...Array(9)].fill(''));
-
-  const _render = () => {
-    console.log(gameBoard);
-    gridItems.forEach(
-      (item) => (item.textContent = gameBoard[item.dataset.position])
-    );
-  };
-
-  const clickOnBoard = () => {
-    gridItems.forEach((item) =>
-      item.addEventListener('click', function () {
-        const position = this.dataset.position;
-
-        // Returns if the cell is already filled
-        if (gameBoard[position]) return;
-
-        gameBoard[position] = GamePlay.currentPlayer().marker;
-        GamePlay.currentPlayer().moveTracker.push(position);
-        _render();
-        GamePlay.switchPlayer();
-      })
-    );
-  };
-
-  return { clickOnBoard };
-})();
-
-const PlayerFactory = (name, marker, isPlaying = false) => {
-  let moveTracker = [];
-  return { name, marker, isPlaying, moveTracker };
+const players = (name, marker) => {
+  return { name, marker };
 };
 
-const player1 = PlayerFactory('Mugilan', 'X', true);
-const player2 = PlayerFactory('Jay', 'O');
+const gameBoard = (function () {
+  let board = ['', '', '', '', '', '', '', '', ''];
 
-console.log({ player1, player2 });
-
-const GamePlay = (function () {
-  let currentPlayer = () => (player1.isPlaying ? player1 : player2);
-
-  const switchPlayer = () => {
-    player1.isPlaying = !player1.isPlaying;
-    player2.isPlaying = !player2.isPlaying;
+  const updateBoard = (marker, position) => {
+    if (board[position] !== '') return;
+    board[position] = marker;
+    console.log(board);
   };
 
-  return { switchPlayer, currentPlayer };
+  return { updateBoard };
 })();
 
-GameBoard.clickOnBoard();
+const displayController = (function () {})();
